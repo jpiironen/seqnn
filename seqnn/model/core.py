1d4b87f3a66adadc16ed35771318f3463093c9e1
Juho Piironen <juho.piironen@topdatascience.com>
2022-10-28 16:53:21 +0300

    Add model core for non-linear dynamical system

--- seqnn/model/core.py
+++ seqnn/model/core.py
@@ -2,8 +2,9 @@ import pathlib
 import torch
 import torch.nn as nn
 import seqnn
+from seqnn.model.basemodels import MLP, CNN1d
 from seqnn.model.transformer import GenerativeTransformer
-from seqnn.utils import get_cls
+from seqnn.utils import get_cls, ensure_list
 
 
 class ModelCore(nn.Module):
@@ -47,13 +48,127 @@ class ModelCore(nn.Module):
     def get_num_outputs(self):
         return self.likelihood.get_num_parameters() * self.num_target
 
+    def split_past_and_future(self, tensor):
+        if tensor is None:
+            return None, None
+        return torch.split(tensor, (self.horizon_past, self.horizon_future), dim=1)
+
     def forward(
         self, target_past, control_past, control_future, target_future=None, **kwargs
     ):
         raise NotImplementedError
 
-    def get_loss(self, target, control):
-        raise NotImplementedError
+    def get_loss(self, target, control, aux=None, teacher_forcing=False):
+        target_past, target_future = self.split_past_and_future(target)
+        control_past, control_future = self.split_past_and_future(control)
+        aux_past, _ = self.split_past_and_future(aux)
+        output = self(
+            target_past,
+            control_past,
+            control_future,
+            aux_past=aux_past,
+            target_future=target_future,
+            teacher_forcing=teacher_forcing,
+        )
+        losses = self.likelihood.get_loss(output, target_future)
+        return losses
+
+
+class NLDS(ModelCore):
+    """Non-linear dynamical system (NLDS), where the intial latent state
+    is estimated using a CNN, and dynamics are computed via an MLP"""
+
+    def __init__(
+        self,
+        likelihood,
+        num_target,
+        num_control,
+        horizon_past,
+        horizon_future,
+        num_auxiliary=0,
+        num_filters=[64, 64],
+        num_hidden_dynamics=[128],
+        num_hidden_readout=[128],
+        kernel_size=3,
+        latent_size=32,
+        dropout=0.1,
+        dropout_latent=0.0,
+        act=nn.ReLU(),
+    ):
+        super().__init__(
+            likelihood,
+            num_target,
+            num_control,
+            horizon_past,
+            horizon_future,
+        )
+        num_filters = ensure_list(num_filters)
+        num_hidden_dynamics = ensure_list(num_hidden_dynamics)
+        num_hidden_readout = ensure_list(num_hidden_readout)
+        seq_len_enc = horizon_past
+        self.encoder = CNN1d(
+            seq_len=seq_len_enc,
+            conv_sizes=[num_control + num_target + num_auxiliary] + num_filters,
+            fc_sizes=[latent_size],
+            kernel_size=kernel_size,
+            act=act,
+            dropout=dropout,
+        )
+        self.dynamics_model = MLP(
+            [latent_size + num_control] + num_hidden_dynamics + [latent_size],
+            dropout=dropout,
+            act=act,
+        )
+        self.dropout_latent = nn.Dropout(dropout_latent)
+        self.readout_model = MLP(
+            [latent_size] + num_hidden_readout + [self.num_output],
+            dropout=dropout,
+            act=act,
+        )
+
+    def encode(self, control_past, target_past, aux_past=None):
+        # encode the past sequences using CNN to estimate the current latent state
+        if aux_past is not None:
+            x = torch.cat((target_past, control_past, aux_past), dim=2)
+        else:
+            x = torch.cat((target_past, control_past), dim=2)
+        x = x.permute(0, 2, 1)
+        state = self.encoder(x)
+        return state
+
+    def forward_one_step(self, state, control):
+        assert state.ndim == 2
+        assert control.ndim == 2
+        x = torch.cat((state, control), dim=1)
+        return state + self.dynamics_model(x)
+
+    def forward(
+        self,
+        target_past,
+        control_past,
+        control_future,
+        target_future=None,
+        aux_past=None,
+        **kwargs
+    ):
+        # encode the past, and unroll the predictions about how the
+        # hidden state evolves, and finally map the hidden states to target predictions
+        state = self.encode(control_past, target_past, aux_past)
+        control_all = torch.cat((control_past, control_future), dim=1)
+        batch_size, horizon_future, num_control = control_future.shape
+        hidden_states = []
+        for i in range(horizon_future):
+            control = control_all[
+                :,
+                self.horizon_past + i : self.horizon_past + i + 1,
+                :,
+            ].view(batch_size, -1)
+            state = self.forward_one_step(state, control)
+            hidden_states.append(state.view(batch_size, 1, -1))
+        hidden_states = torch.cat(hidden_states, dim=1)
+        hidden_states = self.dropout_latent(hidden_states)
+        output = self.readout_model(hidden_states)
+        return output
 
 
 class RNN(ModelCore):
@@ -147,7 +262,7 @@ class Transformer(ModelCore):
             dropout=dropout,
         )
 
-    def get_loss(self, target, control):
+    def get_loss(self, target, control, **kwargs):
         tokens = target.squeeze(dim=2)
         pred = self.model(tokens)
         # prediction loss for every token (except the first one) conditioned on all preceding tokens
